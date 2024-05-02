e47c850c0fc758b92479e075041b86e0a2fca943
phuctdang <phuctrungdang@gmail.com>
2024-05-02 13:01:16 -0700

    first commit (template)

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react';
+import './App.css';
+import { Routes, Route, Link} from 'react-router-dom'
+
+const BASE_API_URL = "https://fakestoreapi.com/"
+
+function App() {
+
+  return (
+    <>
+      <h1>Under construction...</h1>
+
+
+    </>
+  )
+}
+
+export default App
