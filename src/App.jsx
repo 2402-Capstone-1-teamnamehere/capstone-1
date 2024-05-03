76a7386b018954644b05e3d4d33b6c4e2435c3bb
phuctdang <phuctrungdang@gmail.com>
2024-05-03 09:23:00 -0700

    fixed homepage

--- src/App.jsx
+++ src/App.jsx
@@ -11,6 +11,8 @@ function App() {
   return (
     <>
       <h1>F.I.T.E.M.I</h1>
+      <AllProducts BASE_API_URL={BASE_API_URL}/>
+      
       <Routes>
         <Route path='/products' element={<AllProducts BASE_API_URL={BASE_API_URL}/>} />
         <Route path='products/:ID' element={<SingleProduct BASE_API_URL={BASE_API_URL}/>} />
