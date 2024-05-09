3497c8480982de85489085082c0b8aca7db08357
phuctdang <phuctrungdang@gmail.com>
2024-05-08 22:56:41 -0700

    added final touches

--- src/components/Support.jsx
+++ src/components/Support.jsx
@@ -2,6 +2,7 @@ const Support = () => {
   return (
     <div id="help">
       <img src="https://img.freepik.com/free-vector/flat-design-illustration-customer-support_23-2148887720.jpg?size=338&ext=jpg&ga=GA1.1.553209589.1715126400&semt=ais_user"/>
+      <div id="call">Call: 1-800-PLS-HOLD</div>
     </div>
   )
 }
