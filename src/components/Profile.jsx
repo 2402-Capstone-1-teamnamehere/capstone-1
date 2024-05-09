b11e0648febfba2fd3ba84ebd7988d30fc470eca
phuctdang <phuctrungdang@gmail.com>
2024-05-08 20:47:11 -0700

    updated

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,7 +1,7 @@
 const Profile = () => {
   return (
     <div id="jeff">
-      <img id="jeffimg" src="src/images/mynamejeff.png"/>
+      <img id="jeffimg" src="https://ih1.redbubble.net/image.4735677921.9567/st,small,507x507-pad,600x600,f8f8f8.jpg"/>
     </div>
   )
 }
